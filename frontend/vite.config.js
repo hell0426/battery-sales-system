3bd99d9bda8ede068083ba488beb1d018fc9ff49
hell <lqj2074@qq.com>
2026-02-28 21:30:37 +0800

    feat: 完成商品管理全套增删改查功能

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,4 +15,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+ 
+  server: {
+    port: 5173, // 前端固定运行在 5173 端口
+    proxy: {
+      '/api': { // 只要看到请求是以 /api 开头的
+        target: 'http://localhost:8080', // 就自动转发给后端的 8080 端口
+        changeOrigin: true, // 允许跨域
+        rewrite: (path) => path.replace(/^\/api/, '') // 关键动作：把路径里的 /api 去掉，发给后端真实的 /product/list
+      }
+    }
+  }
 })
